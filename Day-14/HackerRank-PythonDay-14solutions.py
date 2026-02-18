eeb4c792e38a239206232f0a283e05cf73f582c2
Kadir Demir <demirabdulkadir04@gmail.com>
2026-02-18 09:43:46 +0300

    solutions updated

--- Day-14/HackerRank-PythonDay-14solutions.py
+++ Day-14/HackerRank-PythonDay-14solutions.py
@@ -86,6 +86,28 @@ parser = MyHTMLParser()
 for _ in range(int(input())):
     parser.feed(input())
 
+# ---------------------------------------------------------
+# PROBLEM 4: XML 1 - Find the Score
+# ---------------------------------------------------------
+import sys
+import xml.etree.ElementTree as etree
+
+
+def get_attr_number(node):
+    count = len(node.attrib)
+
+    for child in node:
+        count += get_attr_number(child)
+
+    return count
+
+
+if __name__ == '__main__':
+    sys.stdin.readline()
+    xml = sys.stdin.read()
+    tree = etree.ElementTree(etree.fromstring(xml))
+    root = tree.getroot()
+    print(get_attr_number(root))
 
 
 
