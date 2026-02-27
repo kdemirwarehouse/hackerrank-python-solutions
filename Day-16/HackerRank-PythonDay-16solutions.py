bd2028207eed782ea387e861c440866fd03ce273
Kadir Demir <demirabdulkadir04@gmail.com>
2026-02-27 12:00:55 +0300

    Day 16 solutions added

--- Day-16/HackerRank-PythonDay-16solutions.py
+++ Day-16/HackerRank-PythonDay-16solutions.py
@@ -21,3 +21,25 @@ arr = np.array(list1)
 print(np.transpose(arr))
 print(arr.flatten())
 
+# ---------------------------------------------------------
+# PROBLEM 2: Concatenate
+# ---------------------------------------------------------
+import numpy as np
+
+N, M, P = map(int, input().split())
+
+list1 = []
+list2 = []
+
+for i in range(N):
+    a = list(map(int, input().split()))
+    list1.append(a)
+
+for i in range(M):
+    b = list(map(int, input().split()))
+    list2.append(b)
+
+arr1 = np.array(list1)
+arr2 = np.array(list2)
+
+print(np.concatenate((arr1, arr2), axis=0))
