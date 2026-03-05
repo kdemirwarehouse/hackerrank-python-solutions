90ae368edbddf4d920032cea1b336bcfc58e2013
Kadir Demir <demirabdulkadir04@gmail.com>
2026-03-05 09:42:46 +0300

    Day 17 solutions updated

--- Day-17/HackerRank-PythonDay-17solutions.py
+++ Day-17/HackerRank-PythonDay-17solutions.py
@@ -15,3 +15,19 @@ print(np.floor(A))
 print(np.ceil(A))
 print(np.rint(A))
 
+# ---------------------------------------------------------
+# PROBLEM 2: Sum and Prod
+# ---------------------------------------------------------
+import numpy as np
+
+N, M = map(int,input().split())
+
+my_list = []
+
+[my_list.append(list(map(int, input().split()))) for _ in range(N)]
+
+my_array = np.array(my_list)
+
+a = np.sum(my_array, axis = 0)
+
+print(np.prod(a, axis=None))
