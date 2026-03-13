092423fcdb4ce79e5f6b4a1873a07ba252c6061c
Kadir Demir <demirabdulkadir04@gmail.com>
2026-03-13 10:33:44 +0300

    Implement linear algebra determinant calculation
    Added a new problem for calculating the determinant of a matrix using NumPy.

--- Day-18/HackerRank-PythonDay-18solutions.py
+++ Day-18/HackerRank-PythonDay-18solutions.py
@@ -17,6 +17,22 @@ x = int(input())
 
 print(np.polyval(arr_p, x))
 
+# ---------------------------------------------------------
+# PROBLEM 2: Linear Algebra
+# ---------------------------------------------------------
+import numpy as np
+
+N = int(input())
+
+my_list = []
+
+for _ in range(N):
+    my_list.append(list(map(float, input().split())))
+
+A = np.array(my_list)
+
+print(np.linalg.det(A))
+
 
 
 
