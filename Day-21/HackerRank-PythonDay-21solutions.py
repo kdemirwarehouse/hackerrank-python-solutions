8659962aa1600bb961d0d09b1e7fe4756bced235
Kadir Demir <demirabdulkadir04@gmail.com>
2026-05-06 22:43:31 +0300

    Fix syntax error and implement Complex class methods

--- Day-21/HackerRank-PythonDay-21solutions.py
+++ Day-21/HackerRank-PythonDay-21solutions.py
@@ -7,4 +7,88 @@ Date: 2025-05-04
 # PROBLEM 1: Triangle Quest
 # ---------------------------------------------------------
 for i in range(1, int(input())):
-    print(i * (10**i - 1) // 9)
+    print(i * (10**i - 1) // 9
+
+# ---------------------------------------------------------
+# PROBLEM 2: Classes: Dealing with Complex Numbers
+# ---------------------------------------------------------
+import math
+class Complex(object):
+    def __init__(self, real, imaginary):
+        self.real = real
+        self.imaginary = imaginary
+        
+    def __add__(self, no):
+        return Complex(
+            self.real + no.real,
+            self.imaginary + no.imaginary
+        )
+
+    def __sub__(self, no):
+         return Complex(
+            self.real - no.real,
+            self.imaginary - no.imaginary
+        )
+
+    def __mul__(self, no):
+        return Complex(
+            self.real * no.real - self.imaginary * no.imaginary,
+            self.real * no.imaginary + self.imaginary * no.real
+        )
+
+    def __truediv__(self, no):
+        denominator = no.real**2 + no.imaginary**2
+
+        return Complex(
+            (
+                self.real * no.real +
+                self.imaginary * no.imaginary
+            ) / denominator,
+
+            (
+                self.imaginary * no.real -
+                self.real * no.imaginary
+            ) / denominator
+        )
+
+
+    def mod(self):
+        
+        return Complex(
+            math.sqrt(
+                self.real**2 +
+                self.imaginary**2
+            ),
+            0
+        )
+
+
+    def __str__(self):
+        if self.imaginary == 0:
+            result = "%.2f+0.00i" % (self.real)
+
+        elif self.real == 0:
+            if self.imaginary >= 0:
+                result = "0.00+%.2fi" % (self.imaginary)
+            else:
+                result = "0.00-%.2fi" % (abs(self.imaginary))
+
+        elif self.imaginary > 0:
+            result = "%.2f+%.2fi" % (
+                self.real,
+                self.imaginary
+            )
+
+        else:
+            result = "%.2f-%.2fi" % (
+                self.real,
+                abs(self.imaginary)
+            )
+
+        return result
+if __name__ == '__main__':
+    c = map(float, input().split())
+    d = map(float, input().split())
+    x = Complex(*c)
+    y = Complex(*d)
+    print(*map(str, [x+y, x-y, x*y, x/y, x.mod(), y.mod()]), sep='\n')
