5054d4357d0c7482107c8ef28043517317dea454
Kadir Demir <demirabdulkadir04@gmail.com>
2026-06-09 16:20:59 +0300

    Update Day-22.py

--- Day-22/Day-22.py
+++ Day-22/Day-22.py
@@ -51,3 +51,15 @@ for _ in range(int(input())):
         print("Valid")
     else:
         print("Invalid")
+
+# ---------------------------------------------------------
+# PROBLEM 4: Validating Credit Card Numbers
+# ---------------------------------------------------------
+regex_integer_in_range = r"^[1-9][0-9]{5}$"	
+regex_alternating_repetitive_digit_pair = r"(\d)(?=\d\1)"	
+
+import re
+P = input()
+
+print (bool(re.match(regex_integer_in_range, P)) 
+and len(re.findall(regex_alternating_repetitive_digit_pair, P)) < 2)
